5ab52ac3512ea0fa10457f8d1cff42e04accea2a
Dario Izzo <dario.izzo@gmail.com>
2019-09-20 10:45:06 +0200

    fix for outdated package

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,8 @@ mkdir build
 cd build
 
 cmake ^
-    -G "%CMAKE_GENERATOR%" ^
+    -G "NMake Makefiles" ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DAUDI_BUILD_AUDI=yes \
